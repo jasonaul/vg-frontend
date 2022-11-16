09b967ae31e7f527ee32a7c3774b29aa9796dc6b
jasonaul <jason.aul@gmail.com>
2022-11-16 15:33:51 -0500

    frontend testing for vgtourism

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
+
+
+import UsersPage from './user/pages/Users'
+import NewDestination from './destinations/pages/NewDestination'
+import Navbar from './shared/components/Navigation/Navbar'
+import NavbarTrue from './shared/components/Navigation/NavbarTrue'
+
+function App() {
+  return <Router>
+
+  <NavbarTrue />
+<main>
+<Switch>
+
+    <Route path="/users" exact>
+      <UsersPage />
+    </Route>
+
+    <Route path="/destinations/new" exact>
+      <NewDestination />
+    </Route>
+
+    <Redirect to="/" /> {/* If no route is hit, it will automatically redirect home. Change this path later if you want it to redirect to a "Not Found" style page*/}
+
+</Switch>
+</main>
+  </Router>
+}
+
+export default App
